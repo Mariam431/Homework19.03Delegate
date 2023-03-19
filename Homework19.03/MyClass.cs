9f31c9772328a59629d9276eefc7d32ba8e60e03
mariam <mariam@MYPROBOOK>
2023-03-19 22:26:15 +0400

    Add project files.

---
+++ Homework19.03/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework19._03
+{
+    public delegate int MyDelegate(int num);
+    internal class MyClass
+    {
+        public int MethA (int num)
+        {
+            if (num == 1 && num == 0)
+            {
+                return 1;
+            }
+            while (num > 1)
+                return num * MethA(num - 1);
+            return 1;
+        }
+        public int MethB(int num)
+        {
+            Console.WriteLine(num+1);
+            return num;
+        }
+        public int MethC(int num)
+        {
+            Console.WriteLine(num + 2);
+            return num;
+        }
+        public int MethD(int num)
+        {
+            Console.WriteLine(num + 3);
+            return num;
+        }
+        public int MethE(int num)
+        {
+            Console.WriteLine(num + 4);
+            return num;
+        }
+    }
+}
---
+++ Homework19.03/Program.cs
@@ -0,0 +1,22 @@
+﻿using static Homework19._03.MyClass;
+namespace Homework19._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyDelegate myDelegate = new MyDelegate(myClass.MethA);
+            myDelegate += myClass.MethB;
+            myDelegate += myClass.MethC;
+            myDelegate += myClass.MethD;
+            myDelegate += myClass.MethE;
+            myDelegate(5);
+            Console.WriteLine(myClass.MethA(5));// vonc kanchem MethA-n? inchi delegate-y arjeqavoreluc inqy chi arjeqavorvum u petqa arancin cw-i mej iran arjeqavorel?
+
+
+
+
+        }
+    }
+}
